505a5dcbb900a993766266993546157f6b6ecaa9
unsinkableoscar <asafronov.bryansk@gmail.com>
2022-02-04 03:58:07 -0800

    Edit start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-inventory=/home/alles/ansible-cm/inventory/hosts.ini
-=======
+
 inventory=/home/alles/ansible_cm/inventory/hosts.ini
->>>>>>> lab_1
+
 
 ansible-playbook playbook.yml -i $inventory -kK -u alles -vv
