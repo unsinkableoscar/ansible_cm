7cfd8bc0e5e5c6a869477d27766451a73aca47ca
unsinkableoscar <asafronov.bryansk@gmail.com>
2022-02-17 04:07:40 -0800

    edit configs

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 inventory=/home/alles/ansible_cm/inventory/hosts.ini
 
 
-ansible-playbook playbook.yml -i $inventory -kK --ask-vault-pass -u alles -vv "$@"
+ansible-playbook playbook.yml -i $inventory -kK --ask-vault-pass -u alles -vvv "$@"
